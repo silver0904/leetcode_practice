cd3dd65f1cf6050f7a0de39e2a31aeea87da3fb4
Armand Chung <homingchung0904@gmail.com>
2022-07-05 20:48:30 -0700

    add RandomBehavioralQuestion

--- src/main.java
+++ src/main.java
@@ -1,3 +1,4 @@
+import amazon.RandomBehavioralQuestion;
 import easy.*;
 import medium.*;
 
@@ -5,11 +6,7 @@ import java.util.*;
 
 public class main {
     public static void main (String arg[]){
-        System.out.println(Q300LongestIncreasingSubsequence.lengthOfLIS2(
-                new int[]{0,1,0,3,2,3}
-        ));
-        Set<String>[] dp = new HashSet[10];
-        dp[0]= Set.of("()");
+        System.out.println(RandomBehavioralQuestion.pickQuestion());
 
         //System.out.println(test.minimalHeaviestSetA(new ArrayList<>(List.of(5,3,2,4,1,2))));
         return;
--- src/test.java
+++ src/test.java
@@ -3,8 +3,5 @@ import java.util.stream.Collectors;
 
 public class test {
     public void test(String p) {
-        Map<Character, Integer> taskCountMap = new HashMap<>();
-        String s = "";
-
     }
 }
